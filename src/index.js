c0f2c67a961e542da5d9c361fd6a693f5554faaa
Venicyus Venceslencio <venicyusti@gmail.com>
2018-08-24 10:17:42 -0300

    Novos makers

--- src/index.js
+++ src/index.js
@@ -1,122 +1,40 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { renderToStaticMarkup } from 'react-dom/server';
 import registerServiceWorker from './registerServiceWorker';
-import Svg from './svg';
-import './index.css';
-import html2canvas from 'html2canvas-webpack';
 
-const limiteMax = 350;
+import { renderToStaticMarkup } from 'react-dom/server';
+import html2canvas from 'html2canvas-webpack';
+import SvgBadge from './svg-badge';
 
 class App extends Component {
 
     constructor() {
         super();
-        this.state = { markers: [] };
         this.onSubmit = this.download.bind(this);
-        this.markers = [];
     }
 
     download(e) {
         e.preventDefault();
-        var self = this;
-        console.log('Processando...');
 
-        if (false) {
-            fetch('http://localhost:5000/api/SvgToPng', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(self.markers)
-            }).then((res) => {
-                res.json().then((url) => {
-                    console.log('Concluído');
-                    let link = document.createElement("a");
-                    link.download = "markers.zip";
-                    link.href = url;
-                    console.log(url);
-                    link.click();
-                    console.log('Concluído...');
-                });
-            });
-        } else {
-            html2canvas(document.getElementById("imgs")).then((canvas) => {
-                let link = document.createElement("a");
-                link.download = "pin_label.png";
-                link.href = canvas.toDataURL();
-                link.click();
-                console.log('Concluído...');
-            });
-        }
+        html2canvas(document.getElementById("imgs")).then((canvas) => {
+            let link = document.createElement("a");
+            link.download = "pin_label.png";
+            link.href = canvas.toDataURL();
+            link.click();
+        });
     }
 
     render() {
         let imagens = [];
-        let cores = [
-            { background: '#037029', borderColor: '#000000', color: '#FFF' },
-            { background: '#009FFD', borderColor: '#000000', color: '#FFF' },
-            { background: '#BB0A21', borderColor: '#000000', color: '#FFF' },
-            { background: '#FFA400', borderColor: '#000000', color: '#FFF' },
-            { background: '#841B67', borderColor: '#000000', color: '#FFF' },
-            { background: '#3D005F', borderColor: '#000000', color: '#FFF' },
-            { background: '#000000', borderColor: '#000000', color: '#FFF' },
-
-            { background: '#68E991', borderColor: '#000000', color: '#000' },
-            { background: '#8DC0EB', borderColor: '#000000', color: '#000' },
-            { background: '#EB9292', borderColor: '#000000', color: '#000' },
-            { background: '#EBD476', borderColor: '#000000', color: '#000' },
-            { background: '#DB63BA', borderColor: '#000000', color: '#000' },
-            { background: '#A89ECB', borderColor: '#000000', color: '#000' },
-            { background: '#AAAAAA', borderColor: '#000000', color: '#000' }
-        ];
-
-        for (let j = 0; j < cores.length; j++) {
-
-            let config = {
-                background: cores[j].background,
-                borderColor: cores[j].borderColor,
-                labelColor: cores[j].color
-            };
-
-            for (let i = 0; i <= limiteMax; i++) {
-                if (i !== 0) {
-                    config.label = i;
-                    if (i > 0 && i < 10) {
-                        config.labelX = 6.6;
-                        config.labelY = 14;
-                        config.labelSize = '14px';
-                    } else if (i >= 10 && i < 100) {
-                        config.labelX = 2.5;
-                        config.labelY = 14;
-                        config.labelSize = '14px';
-                    } else if (i >= 100 && i < limiteMax) {
-                        config.labelX = 1.6;
-                        config.labelY = 13;
-                        config.labelSize = '11px';
-                    } else {
-                        config.label = "+";
-                        config.labelX = 4;
-                        config.labelY = 17;
-                        config.labelSize = '24px';
-                    }
-                }
-
-                const dataUri = 'data:image/svg+xml,' + encodeURIComponent(renderToStaticMarkup(<Svg config={config} />));
-                imagens.push(<img src={dataUri} key={i + (j * 1000)} alt="" />);
-
-                this.markers.push({
-                    data: renderToStaticMarkup(<Svg config={config} />),
-                    name: i === 501 ? 'plus' : i,
-                    folder: cores[j].background
-                });
-            }
-        }
+        const dataUri = 'data:image/svg+xml,' + encodeURIComponent(renderToStaticMarkup(<SvgBadge />));
+        imagens.push(<img src={dataUri} alt="" key="1" />);
 
         return (
             <div>
-                <div id="imgs" style={{ width: 20 * (limiteMax + 1) }}>{imagens}</div>
+                <div id="imgs" style={{ width: 100 }}>
+                    {imagens}
+                </div>
+                <br />
                 <br />
                 <form onSubmit={this.onSubmit}>
                     <input type="submit" value="Baixar" />
@@ -124,6 +42,7 @@ class App extends Component {
             </div >
         );
     }
+
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/svg-badge.js
+++ src/svg-badge.js
@@ -3,28 +3,31 @@ import React, { Component } from 'react';
 export default class SvgBadge extends Component {
     render() {
         return (
-            <div>
-                <svg height="100" width="100">
-                    <circle cx="50" cy="50" r="40" stroke="black" stroke-width={sw} fill={color} />
-                    <text x={x} y={y} font-family="sans-serif" font-size="46" text-anchor="middle" fill={colorLabel}>1</text>
-                </svg>
-                <svg height="100" width="100">
-                    <circle cx="50" cy="50" r="40" stroke="black" stroke-width={sw} fill={color} />
-                    <text x={x} y={y} font-family="sans-serif" font-size="46" text-anchor="middle" fill={colorLabel}>2</text>
-                </svg>
-                <svg height="100" width="100">
-                    <circle cx="50" cy="50" r="40" stroke="black" stroke-width={sw} fill={color} />
-                    <text x={x} y={y} font-family="sans-serif" font-size="46" text-anchor="middle" fill={colorLabel}>3</text>
-                </svg>
-                <svg height="100" width="100">
-                    <circle cx="50" cy="50" r="40" stroke="black" stroke-width={sw} fill={color} />
-                    <text x={x} y={y} font-family="sans-serif" font-size="46" text-anchor="middle" fill={colorLabel}>4</text>
-                </svg>
-                <svg height="100" width="100">
-                    <circle cx="50" cy="50" r="40" stroke="black" stroke-width={sw} fill={color} />
-                    <text x={x + 4} y={y} font-family="sans-serif" font-size="46" text-anchor="middle" fill={colorLabel}>5+</text>
-                </svg>
-            </div >
+            <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="45" cy="52" r="40" stroke="white" strokeWidth="5" fill={'white'} />
+                <circle cx="50" cy="50" r="40" stroke="white" strokeWidth="4" fill="#0e4aa3" />
+                <text x="49" y="70" fontFamily="sans-serif" fontSize="60" textAnchor="middle" fill="white">1</text>
+            </svg>
+            // <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+            //     <circle cx="45" cy="52" r="40" stroke="white" strokeWidth="5" fill={'white'} />
+            //     <circle cx="50" cy="50" r="40" stroke="white" strokeWidth="4" fill="#0da247" />
+            //     <text x="50" y="68" fontFamily="sans-serif" fontSize="60" textAnchor="middle" fill="white">2</text>
+            // </svg>
+            // <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+            //     <circle cx="45" cy="52" r="40" stroke="white" strokeWidth="5" fill={'white'} />
+            //     <circle cx="50" cy="50" r="40" stroke="white" strokeWidth="4" fill="#a10d5c" />
+            //     <text x="50" y="71" fontFamily="sans-serif" fontSize="60" textAnchor="middle" fill="white">3</text>
+            // </svg>
+            // <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+            //     <circle cx="45" cy="52" r="40" stroke="white" strokeWidth="5" fill={'white'} />
+            //     <circle cx="50" cy="50" r="40" stroke="white" strokeWidth="4" fill="#686868" />
+            //     <text x="48" y="70" fontFamily="sans-serif" fontSize="60" textAnchor="middle" fill="white">4</text>
+            // </svg>
+            // <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+            //     <circle cx="45" cy="52" r="40" stroke="white" strokeWidth="5" fill={'white'} />
+            //     <circle cx="50" cy="50" r="40" stroke="white" strokeWidth="4" fill="#af5a1d" />
+            //     <text x="52" y="71" fontFamily="sans-serif" fontSize="60" textAnchor="middle" fill={'white'}>5+</text>
+            // </svg>
         );
     }
 }
